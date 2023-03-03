209faa1ddd1a5ce3b6787d43728da9e0bab8dc62
Desmond Foo <102380796+SHINE-six@users.noreply.github.com>
2023-03-03 07:58:59 +0000

    add file

--- Cost of ballon.c
+++ Cost of ballon.c
@@ -9,6 +9,29 @@ int main(){
         scanf("%d %d",&green, &purple);   
         int participant;
         scanf("%d",&participant);
+        
+        int cost = 0;
+
+        int number_first_test = 0;
+        int number_second_test = 0;
+        for (int x = 0; x < participant; x++)
+        {
+            int first_test, second_test;
+            scanf("%d %d",&first_test, &second_test);
+            number_first_test += first_test;
+            number_second_test += second_test;
+        }
+        int cost1 = (number_first_test * green) + (number_second_test * purple);
+        int cost2 = (number_first_test * purple) + (number_second_test * green);
+        if (cost1 < cost2)
+        {
+            cost = cost1;
+        }
+        else
+        {
+            cost = cost2;
+        }
+        printf("%d\n",cost);
     }
 
 return 0;
