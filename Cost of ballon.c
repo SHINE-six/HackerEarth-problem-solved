028c557ef6e29bb6e8f8cdbd3323a6fef1b772ef
Desmond Foo <102380796+SHINE-six@users.noreply.github.com>
2023-03-03 07:35:07 +0000

    add file

---
+++ Cost of ballon.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+    int T;
+    scanf("%d",&T);
+    for (int i = 0; i < T; i++)
+    {
+        int green, purple;
+        scanf("%d %d",&green, &purple);   
+        int participant;
+        scanf("%d",&participant);
+    }
+
+return 0;
+}
